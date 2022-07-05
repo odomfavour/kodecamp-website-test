0898fb23ae7caf1f98d1299dd1e1b4fd1f8b4d1d
odomfavour <ogochukwuodom@gmail.com>
2022-07-05 21:42:01 +0100

    ch: fix folder structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Index";
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+  );
+}
+
+export default App;
