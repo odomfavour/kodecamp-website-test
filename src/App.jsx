d7e2ecfc5fd0dcc28e6a2ea4de330148bf8278ff
veedahv <veedahv55@gmail.com>
2022-07-10 21:48:53 +0100

    ft: created EnrollNow page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import EnrollNow from "./components/EnrollNow/EnrollNow";
 import Home from "./pages/Index";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/enroll" element={<EnrollNow />} />
       </Routes>
   );
 }
-
 export default App;
