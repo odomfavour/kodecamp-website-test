661c2a1c66e8eb98186fabb452ba7672234743f9
RachelOpuba <ogbenirach@gmail.com>
2022-07-14 21:58:44 +0100

    Removed ExploreVideo component from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,19 @@ import EnrollNow from "./components/EnrollNow/EnrollNow";
 import Home from "./pages/Index";
 import Works from "./pages/Works/Works";
 import Sponsor from "./pages/sponsor/Sponsor";
-import Explorevideo from "./pages/exploreVideo/Explorevideo";
+
 import ContactUs from "./pages/ContactUsPage/ContactUs";
 import Explore from "./pages/ExplorePage/Explore";
-
-
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/enroll" element={<EnrollNow />} />
-      <Route path="/contactus" element={<ContactUs/>} />
-      <Route path="/explore" element={<Explore/>} />
+      <Route path="/contactus" element={<ContactUs />} />
+      <Route path="/explore" element={<Explore />} />
       <Route path="/works" element={<Works />} />
       <Route path="/sponsor" element={<Sponsor />} />
-      <Route path="/explorevideo" element={<Explorevideo/>} />
     </Routes>
   );
 }
