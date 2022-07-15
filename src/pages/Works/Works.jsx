a69c2570a7ce0dcabad6104164865c0d9e19f6a7
danutibe07 <danielutibe07@gmail.com>
2022-07-15 15:23:19 +0100

    added steps on how it works

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -1,10 +1,13 @@
 import KodeCamp30 from "./KodeCamp30";
+import Howitworks from "./Howitworks";
 import "./Works.css";
+import "./Howitworks.css"
 import PageWrapper from "../../layout/PageWapper/PageWrapper";
 
 const Works = () => {
     return (
         <PageWrapper>
+            <Howitworks />
             <KodeCamp30 />
         </PageWrapper>
         
