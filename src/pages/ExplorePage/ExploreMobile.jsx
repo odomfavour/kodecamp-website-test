f4666589f4b274edeb3f6fecedac52681fbfafc3
Babatunde-Ben <babatundeben110@gmail.com>
2022-07-16 11:28:45 +0100

    populated explore page with appropriate track details

--- src/pages/ExplorePage/ExploreMobile.jsx
+++ src/pages/ExplorePage/ExploreMobile.jsx
@@ -1,153 +1,224 @@
-import {Carousel} from 'react-bootstrap';
-import ExploreCard from './ExploreCard';
+import { Carousel } from "react-bootstrap";
+import ExploreCard from "./ExploreCard";
+import Tracks from "./TrackData";
 
 function ExploreMobile() {
   return (
     <>
-        <div className='d-block d-sm-none '>
-                {/* front end development track */}
-                <section className='mt-4'>
-                    <div className='pt-4 container'>
-                        <div className='container'>
-                            <p className='explore-fs-24-700 fw-bold deep-blue-100'>FrontEnd Development</p>
-                        </div>
-                    </div>
-                    <Carousel  className='pb-4' variant="dark" interval='10000' slide='false' wrap='false' controls='false' indicators='false' aria-hidden='false'>        
-                        <Carousel.Item className =''>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>          
-                    </Carousel>
-                </section>
-                {/* back end development track */}
-                <section className='mt-4'>
-                    <div className='pt-4 container'>
-                        <div className='container'>
-                            <p className='explore-fs-24-700 fw-bold deep-blue-100'>BackEnd Development</p>
-                        </div>
-                    </div>
-                    <Carousel  className='pb-4' variant="dark" interval='10000' slide='false' wrap='false' controls='false' indicators='false' aria-hidden='false'  >
-                        <Carousel.Item className =''>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </section>
-                {/* andriod development track */}
-                <section className='mt-4'>
-                    <div className='pt-4 container'>
-                        <div className='container'>
-                            <p className='explore-fs-24-700 fw-bold deep-blue-100'>Andriod Development</p>
-                        </div>
-                    </div>
-                    <Carousel  className='pb-4' variant="dark" interval='10000' slide='false' wrap='false' controls='false' indicators='false' aria-hidden='false'  >
-                        <Carousel.Item className =''>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </section>
-                {/* project management track */}
-                <section className='mt-4'>
-                    <div className='pt-4 container'>
-                        <div className='container'>
-                            <p className='explore-fs-24-700 fw-bold deep-blue-100'>Project Management</p>
-                        </div>
-                    </div>
-                    <Carousel  className='pb-4' variant="dark" interval='10000' slide='false' wrap='false' controls='false' indicators='false' aria-hidden='false'  >          
-                        <Carousel.Item className =''>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>          
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </section>
-                {/* newly added tracks */}
-                <section className='mt-4'>
-                    <div className='pt-4 container'>
-                        <div className='container'>
-                            <p className='explore-fs-24-700 fw-bold deep-blue-100'>Newly Added</p>
-                        </div>
-                    </div>
-                    <Carousel  className='pb-4' variant="dark" interval='10000' slide='false' wrap='false' controls='false' indicators='false' aria-hidden='false'  >
-                        <Carousel.Item className =''>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>           
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </section>
-                {/* back end development track */}
-                <section className='mt-4'>
-                    <div className='pt-4 container'>
-                        <div className='container'>
-                            <p className='explore-fs-24-700 fw-bold deep-blue-100'>Product Design</p>
-                        </div>
-                    </div>
-                    <Carousel  className='pb-4' variant="dark" interval='10000' slide='false' wrap='false' controls='false' indicators='false' aria-hidden='false'  >
-                        <Carousel.Item className =''>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>            
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className='text-center container d-flex justify-content-center'>
-                                <ExploreCard/>
-                            </div>
-                        </Carousel.Item>            
-                    </Carousel>
-                </section>
-        </div>
+      <div className="d-block d-sm-none mobile-carousel">
+        {/* front end development track */}
+        <section className="mt-4">
+          <div className="pt-4 container">
+            <div className="container">
+              <p className="explore-fs-24-700 fw-bold deep-blue-100">
+                FrontEnd Development
+              </p>
+            </div>
+          </div>
+          <Carousel
+            className="pb-4"
+            variant="dark"
+            interval="7000"
+            slide="false"
+            wrap="false"
+            controls="false"
+            indicators="false"
+            aria-hidden="false"
+          >
+            {Tracks.filter(
+              (item) => item.category === "frontend_development"
+            ).map((item) => (
+              <Carousel.Item key={item.id} className="">
+                <div className="text-center container d-flex justify-content-center">
+                  <ExploreCard
+                    track_image={item.track_image}
+                    track_name={item.track_name}
+                    track_desc={item.track_desc}
+                    track_icon={item.track_icon}
+                  />
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </section>
+        {/* back end development track */}
+        <section className="mt-4">
+          <div className="pt-4 container">
+            <div className="container">
+              <p className="explore-fs-24-700 fw-bold deep-blue-100">
+                BackEnd Development
+              </p>
+            </div>
+          </div>
+          <Carousel
+            className="pb-4"
+            variant="dark"
+            interval="7000"
+            slide="false"
+            wrap="false"
+            controls="false"
+            indicators="false"
+            aria-hidden="false"
+          >
+            {Tracks.filter(
+              (item) => item.category === "backend_development"
+            ).map((item) => (
+              <Carousel.Item key={item.id} className="">
+                <div className="text-center container d-flex justify-content-center">
+                  <ExploreCard
+                    track_image={item.track_image}
+                    track_name={item.track_name}
+                    track_desc={item.track_desc}
+                    track_icon={item.track_icon}
+                  />
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </section>
+        {/* andriod development track */}
+        <section className="mt-4">
+          <div className="pt-4 container">
+            <div className="container">
+              <p className="explore-fs-24-700 fw-bold deep-blue-100">
+                Andriod Development
+              </p>
+            </div>
+          </div>
+          <Carousel
+            className="pb-4"
+            variant="dark"
+            interval="7000"
+            slide="false"
+            wrap="false"
+            controls="false"
+            indicators="false"
+            aria-hidden="false"
+          >
+            {Tracks.filter(
+              (item) => item.category === "android_development"
+            ).map((item) => (
+              <Carousel.Item key={item.id} className="">
+                <div className="text-center container d-flex justify-content-center">
+                  <ExploreCard
+                    track_image={item.track_image}
+                    track_name={item.track_name}
+                    track_desc={item.track_desc}
+                    track_icon={item.track_icon}
+                  />
+                </div>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </section>
+        {/* project management track */}
+        <section className="mt-4">
+          <div className="pt-4 container">
+            <div className="container">
+              <p className="explore-fs-24-700 fw-bold deep-blue-100">
+                Project Management
+              </p>
+            </div>
+          </div>
+          <Carousel
+            className="pb-4"
+            variant="dark"
+            interval="7000"
+            slide="false"
+            wrap="false"
+            controls="false"
+            indicators="false"
+            aria-hidden="false"
+          >
+            {Tracks.filter((item) => item.category === "project_maagement").map(
+              (item) => (
+                <Carousel.Item key={item.id} className="">
+                  <div className="text-center container d-flex justify-content-center">
+                    <ExploreCard
+                      track_image={item.track_image}
+                      track_name={item.track_name}
+                      track_desc={item.track_desc}
+                      track_icon={item.track_icon}
+                    />
+                  </div>
+                </Carousel.Item>
+              )
+            )}
+          </Carousel>
+        </section>
+        {/* newly added tracks */}
+        <section className="mt-4">
+          <div className="pt-4 container">
+            <div className="container">
+              <p className="explore-fs-24-700 fw-bold deep-blue-100">
+                Newly Added
+              </p>
+            </div>
+          </div>
+          <Carousel
+            className="pb-4"
+            variant="dark"
+            interval="7000"
+            slide="false"
+            wrap="false"
+            controls="false"
+            indicators="false"
+            aria-hidden="false"
+          >
+            {Tracks.filter((item) => item.category === "newly_added").map(
+              (item) => (
+                <Carousel.Item key={item.id} className="">
+                  <div className="text-center container d-flex justify-content-center">
+                    <ExploreCard
+                      track_image={item.track_image}
+                      track_name={item.track_name}
+                      track_desc={item.track_desc}
+                      track_icon={item.track_icon}
+                    />
+                  </div>
+                </Carousel.Item>
+              )
+            )}
+          </Carousel>
+        </section>
+        {/* back end development track */}
+        <section className="mt-4">
+          <div className="pt-4 container">
+            <div className="container">
+              <p className="explore-fs-24-700 fw-bold deep-blue-100">
+                Product Design
+              </p>
+            </div>
+          </div>
+          <Carousel
+            className="pb-4"
+            variant="dark"
+            interval="7000"
+            slide="false"
+            wrap="false"
+            controls="false"
+            indicators="false"
+            aria-hidden="false"
+          >
+            {Tracks.filter((item) => item.category === "product_design").map(
+              (item) => (
+                <Carousel.Item key={item.id} className="">
+                  <div className="text-center container d-flex justify-content-center">
+                    <ExploreCard
+                      track_image={item.track_image}
+                      track_name={item.track_name}
+                      track_desc={item.track_desc}
+                      track_icon={item.track_icon}
+                    />
+                  </div>
+                </Carousel.Item>
+              )
+            )}
+          </Carousel>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default ExploreMobile
+export default ExploreMobile;
