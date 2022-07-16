2cddaac2cd55ec6f403f69575767b115df266135
OLAKAFI <olatunbosun.kafi@gmail.com>
2022-07-16 17:18:09 +0100

    explore searchbar input resized

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,7 +7,7 @@ function SearchBar() {
             <div className="row height d-flex justify-content-start align-items-center">
               <div className="col-md-8">
                 <div className="search">
-                  <input type="text" className="form-control input-search searchbar-fs-20-500 fw-normal ps-5" placeholder=" Search for courses"/>
+                  <input type="text" className="form-control searchbar-fs-20-500 fw-normal ps-5" placeholder=" Search for courses"/>
                 </div>            
               </div>         
             </div>
