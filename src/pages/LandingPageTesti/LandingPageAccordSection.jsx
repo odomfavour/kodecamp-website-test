b51403e7d65d6f60f5b232a26186ac0dc671c0f5
OLAKAFI <olatunbosun.kafi@gmail.com>
2022-07-28 12:35:20 +0100

    accordion section responsiveness fix on the landing page

--- src/pages/LandingPageTesti/LandingPageAccordSection.jsx
+++ src/pages/LandingPageTesti/LandingPageAccordSection.jsx
@@ -9,15 +9,19 @@ function LandinPageAccordSection() {
     <>
         <section>
             <div className='container pb-5'>
-                <div className='row d-flex align-items-center'>
-                    <Col className="col-sm-10 col-md-12  col-lg-6 ">
+                <div className='row d-flex align-items-center justify-content-center'>
+                    <Col className="col-10 col-lg-6 ">
                         <div>
-                            <h5 className='fs-6 mb-2 border-bottom land-avail line-height-base text-sm-center  text-lg-start fw-bold border-sm-bottom border-lg-bottom col-lg-8 col-md-10 col deep-blue-100 d-block d-md-none '>AVAILABLE TRACKS</h5>
-                            <h5 className=' land-avail d-none d-md-block  deep-blue-100 fs-4 fw-bold '>AVAILABLE TRACKS</h5>
-                            <h1 className='landing-fs-52  deep-blue-100' style={{fontWeight: "800"}}  >
+                            <h5 className='fs-6 mb-2 border-bottom land-avail line-height-base text-center   fw-bold border-sm-bottom border-lg-bottom col-lg-8 col-md-10 col deep-blue-100 d-block d-md-none '>AVAILABLE TRACKS</h5>
+                            <h5 className=' land-avail d-none d-lg-block  deep-blue-100 fs-4 fw-bold '>AVAILABLE TRACKS</h5>
+                            <h5 className=' land-avail d-none d-md-block d-lg-none  text-center deep-blue-100 fs-4 fw-bold '>AVAILABLE TRACKS</h5>
+                            <h1 className=' d-none d-lg-block landing-fs-52  deep-blue-100' style={{fontWeight: "800"}}  >
                                 <span className='green-100'>Checkout</span>  some of our available tracks 
                             </h1>
-                            <h4 className='d-none d-sm-block  line-height-lg fs-4 fw-normal'>
+                            <h1 className=' d-block d-lg-none landing-fs-52 text-center  deep-blue-100' style={{fontWeight: "600"}}  >
+                                <span className='green-100'>Checkout</span>  some of our available tracks 
+                            </h1>
+                            <h4 className='d-none d-lg-block  line-height-lg fs-4 fw-normal'>
                                 With so much to learn from enroling in 
                                 KodeCamp, these are some of the tracks
                                 available to our interns. To browse through 
@@ -27,7 +31,7 @@ function LandinPageAccordSection() {
                                 </Link>
 
                             </h4>
-                            <h4 className='d-block d-sm-none text-center black-100 line-height-lg fs-5 fw-normal'>
+                            <h4 className='d-block d-lg-none text-center black-100 line-height-lg fs-5 fw-normal'>
                                 With so much to learn from enroling in 
                                 KodeCamp, these are some of the tracks
                                 available to our interns. To browse through 
@@ -39,7 +43,7 @@ function LandinPageAccordSection() {
                             </h4> 
                         </div>
                     </Col>
-                    <Col className="col-sm-10 col-md-12 col-lg-6 mt-4">
+                    <Col className="col-10  col-lg-6 mt-4">
                         {/* {
                                 accordiondata.map(accdata => (
                                     (               
