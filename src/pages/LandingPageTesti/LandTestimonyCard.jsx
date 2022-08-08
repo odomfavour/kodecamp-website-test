50624c9fb1d08687bdeb1ce3e16dc6967c0590d4
OLAKAFI <olatunbosun.kafi@gmail.com>
2022-08-08 01:30:12 +0100

    carousel testimony section updated with new testimonies

--- src/pages/LandingPageTesti/LandTestimonyCard.jsx
+++ src/pages/LandingPageTesti/LandTestimonyCard.jsx
@@ -4,15 +4,15 @@ import "./LandTestimony.css";
 function LandTestimonyCard({id, name, track,description,image}) {   
   return (
     <>
-        <div className=''>
+        <div className='check'>
             <div className=' my-4'>
                 <div className='uniform mb-4' key={id}>
-                    <div className=' dotted position-relative'>
-                        <Card className=' dotted  rounded-2 bg-white-100 ' style={{width:'20px'}}>
-                            <Card.Body className='text-center container-fluid position-relative'>
-                                <div className=' not-dotted '>
-                                    <Card className='  position-absolute top-0 start-0 translate-middle rounded-2 bg-white-100 shadow-sm'>
-                                        <Card.Body className='text-center'>
+                    <div className='one dotted position-relative'>
+                        <Card className='two dotted  rounded-2  ' style={{width:'20px'}}>
+                            <Card.Body className='three text-center container-fluid position-relative'>
+                                <div className='four not-dotted '>
+                                    <Card className=' five position-absolute top-0 start-0 translate-middle rounded-2 bg-white-100 shadow-sm'>
+                                        <Card.Body className='six text-center'>
                                             <div className=''>
                                                 <Card.Text className=''>
                                                     <div className=' mx-auto '>
@@ -28,8 +28,8 @@ function LandTestimonyCard({id, name, track,description,image}) {
                                                 </div>
                                             </div>
                                             <div>
-                                                <p className='fw-bold  lh-sm black-100 testimony-fs-name '>{name}</p>
-                                                <p className='testimony-fs-name lh-sm fw-bold blue-100 mt-0'>{track} </p>
+                                                <p className='fw-bold track-lh black-100 testimony-fs-name '>{name}</p>
+                                                <p className='testimony-fs-name track-lh fw-bold blue-100 mt-0'>{track} </p>
                                             </div>
                                         </Card.Body>
                                     </Card>
